5c593ec07781ddc3f70d1bd11534fc521443d4d6
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2025-02-09 20:21:22 +0530

    user verification done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
-import momgoose from "momgoose";
-
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -12,6 +13,23 @@ const userSchema = new mongoose.Schema({
     },
     password:{
         type:String,
-        required: true
+        required: true,
+        select : false,
     }
 });
+
+userSchema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+userSchema.methods.isValidPassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateJWT = function(){
+    return jwt.sign({email: this.email},process.env.JWT_SECRET)
+}
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
