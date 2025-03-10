c9cc4584cb998929252c4ae8ce79e1eae00eadf0
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2025-03-10 15:28:17 +0530

    socketio setup done.

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import http from "http";
+import { Server } from "socket.io"
 
 import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 
 const server = http.createServer(app);
+
+const io = new Server(server);
+io.on('connection', socket  => {
+  console.log('user Connected')
+});
 
 server.listen(port, function(){
     console.log(`Server is running on port ${port}`); 
