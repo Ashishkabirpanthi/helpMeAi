be73d0c6d1302b5525fd829dd2863e438e2dabbf
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2025-03-12 14:00:23 +0530

    fixing bugs in UserAuth

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
+
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
